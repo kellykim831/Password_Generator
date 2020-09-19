d0d3d8538f27305448f78cb60572b82baccba6ce
Kelly Kim <kellykim408@gmail.com>
2020-09-19 00:54:15 -0700

    create password length prompt

--- script.js
+++ script.js
@@ -4,14 +4,35 @@ var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var special =["!","@","#","$","%","^","&","*","/","?","+"];
 
-//Declare variables
-var confirmLength = "";
-var confirmNum;
-var confirmLower;
-var confirmUpper;
-var confirmSpecial;
+
+//Prompt to generate how many characters for password
+function generatePassword() {
+var passwordLength = prompt("How many characters would you like your password to contain?");
+  
+//Loop back if answer is not within these parameters
+if (passwordLength < 8) {
+  alert("Password length must be at least 8 characters");
+  return false
+} else if (passwordLength > 128) {
+  alert("Password length must be less than 129 characters");
+  return false
+}
+
+else if (passwordLength >= 8 || passwordLength <= 128 ) {
+
+var confirmSpecial = confirm("Click OK to confirm including special characters");
+var confirmNum = confirm("Click OK to confirm including numberic characters");
+var confirmLower = confirm("Click OK to confirm including lowercase characters");
+var confirmUpper = confirm("Click OK to confirm including uppercase characters");
+}
 
 
+//Loop if none are confirmed
+  while (confirmSpecial != true && confirmNum != true && confirmLower != true && confirmUpper != true) {
+   alert("Must select at least one character type");
+   return false;
+ }
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -24,8 +45,23 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
