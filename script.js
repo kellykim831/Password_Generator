fe1eee0343f671bf4e720fe6d4365bf92009fb4e
Kelly Kim <kellykim408@gmail.com>
2020-09-18 21:36:30 -0700

    declare variables

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+//Arrays for password generation
 var num = ["0","1","2","3","4","5","6","7","8","9"];
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var special =["!","@","#","$","%","^","&","*","/","?","+"];
+
+//Declare variables
+var confirmLength = "";
+var confirmNum;
+var confirmLower;
+var confirmUpper;
+var confirmSpecial;
+
 
 
 // Assignment Code
